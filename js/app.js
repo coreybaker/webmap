05d396704e3075a64862ba97e81f8ec13b5ee36d
= <coreydavidbaker@gmail.com>
2014-01-23 08:38:01 -0600

    got the webmap in

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 //created by Corey Baker
 //with ok coding practices and lots of heart
 
+//user configurations - beta 
+var webmapID = "ec334b4036f64ba8bb4155f517d4eb66";
+//1a40fa5cc1ab4569b79f45444d728067
+//ec334b4036f64ba8bb4155f517d4eb66
+
+//end global configurations 
+
+
+require([
+"esri/map",
+"esri/arcgis/utils",
+"esri/arcgis/Portal", 
+"dojo/ready", 
+"dojo/parser"
+
+],
+
+
+function(Map, Utils, Portal, Ready, Parser){
+
+
+Utils.createMap(webmapID, "map").then(function(response){
+	Map = response.map;
+});
+
+
+}); //end requires 
